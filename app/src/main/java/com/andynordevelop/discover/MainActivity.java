97b7af887ade2722b2f308601b47875e3c6f901e
andreas <asekkingstad@gmail.com>
2018-08-27 21:41:16 +0200

    first commit

---
+++ app/src/main/java/com/andynordevelop/discover/MainActivity.java
@@ -0,0 +1,112 @@
+package com.andynordevelop.discover;
+
+import android.Manifest;
+import android.content.Context;
+import android.content.pm.PackageManager;
+import android.location.Location;
+import android.location.LocationManager;
+import android.os.Bundle;
+import android.support.annotation.NonNull;
+import android.support.design.widget.FloatingActionButton;
+import android.support.design.widget.Snackbar;
+import android.support.v4.app.ActivityCompat;
+import android.support.v7.app.AppCompatActivity;
+import android.support.v7.widget.Toolbar;
+import android.util.Log;
+import android.view.View;
+import android.view.Menu;
+import android.view.MenuItem;
+
+import java.util.prefs.Preferences;
+
+public class MainActivity extends AppCompatActivity {
+    LocationManager locationManager;
+    Location myLocation;
+    double lat;
+    double lon;
+    boolean useLocation = false;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+        Toolbar toolbar = (Toolbar) findViewById(R.id.toolbar);
+        locationManager = (LocationManager) getSystemService(Context.LOCATION_SERVICE);
+        //Empty location to avoid Nullpointer
+        myLocation = new Location("");
+        //default location it rains in africa :)
+        lat = 0;
+        lon = 0;
+
+
+        setSupportActionBar(toolbar);
+
+        FloatingActionButton fab = (FloatingActionButton) findViewById(R.id.fab);
+        fab.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Snackbar.make(view, "Location finder activated", Snackbar.LENGTH_SHORT)
+                        .setAction("Action", null).show();
+                findNewLocation();
+                Snackbar.make(view, "Location : " + lat + " # " + lon, Snackbar.LENGTH_LONG)
+                        .setAction("Action", null).show();
+            }
+        });
+    }
+
+    private void findNewLocation() {
+        Log.d("Start", "Whereami?");
+        if (ActivityCompat.checkSelfPermission(this, Manifest.permission.ACCESS_FINE_LOCATION) != PackageManager.PERMISSION_GRANTED && ActivityCompat.checkSelfPermission(this, Manifest.permission.ACCESS_COARSE_LOCATION) != PackageManager.PERMISSION_GRANTED) {
+            // TODO: Consider calling
+            //    ActivityCompat#requestPermissions
+            // here to request the missing permissions, and then overriding
+            //   public void onRequestPermissionsResult(int requestCode, String[] permissions,
+            //                                          int[] grantResults)
+            // to handle the case where the user grants the permission. See the documentation
+            // for ActivityCompat#requestPermissions for more details.
+
+        }
+        myLocation = locationManager.getLastKnownLocation(LocationManager.GPS_PROVIDER);
+        lat = myLocation.getLatitude();
+        lon = myLocation.getLongitude();
+        Log.d("Location", ""+lat+":"+lon);
+        if (lat == 0 && lon == 0) {
+            //provide New York
+        }
+    }
+
+    @Override
+    public void onRequestPermissionsResult(int requestCode, @NonNull String[] permissions, @NonNull int[] grantResults) {
+        switch (requestCode) {
+            case 1: {
+                if (grantResults.length > 0 && grantResults[0] == PackageManager.PERMISSION_GRANTED) {
+                    useLocation = true;
+                } else {
+                    //Not allowed!!!
+                }
+            }
+        }
+    }
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        // Inflate the menu; this adds items to the action bar if it is present.
+        getMenuInflater().inflate(R.menu.menu_main, menu);
+        return true;
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+        // Handle action bar item clicks here. The action bar will
+        // automatically handle clicks on the Home/Up button, so long
+        // as you specify a parent activity in AndroidManifest.xml.
+        int id = item.getItemId();
+
+        //noinspection SimplifiableIfStatement
+        if (id == R.id.action_settings) {
+            return true;
+        }
+
+        return super.onOptionsItemSelected(item);
+    }
+}
